0d8f598210d11bb17df0056576a66b5bb9e682b1
nils <26379736+NiGour@users.noreply.github.com>
2023-09-29 16:19:55 +0200

    added interests

---
+++ script.js
@@ -0,0 +1,7 @@
+// let sectionDeroulante=document.getElementsByClassName("experience");
+
+// document.addEventListener("click",console.log(this));
+// window.onclick=e=>{
+//     console.log(e.target.set);
+// }
+// console.log(sectionDeroulante)
