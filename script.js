c1459e39e2d57f7b09ff13bbd706ae182e1f994d
nils <26379736+NilsGour@users.noreply.github.com>
2024-04-08 00:04:52 +0200

    modified page structure

--- script.js
+++
@@ -1,7 +0,0 @@
-// let sectionDeroulante=document.getElementsByClassName("experience");
-
-// document.addEventListener("click",console.log(this));
-// window.onclick=e=>{
-//     console.log(e.target.set);
-// }
-// console.log(sectionDeroulante)
